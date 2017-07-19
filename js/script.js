55574f18e90b13fdcd7710f2412b0dc69bb4980f
Vincent Tang <vincet3@uci.edu>
2017-07-19 15:11:41 -0700

    modify background image and fixed parallax

--- js/script.js
+++ js/script.js
@@ -8,12 +8,12 @@ document.onreadystatechange = function () {
          document.getElementById('preloader').style.visibility="hidden";
          document.getElementById('contents').style.visibility="visible";
       },1000);
+      if ($('.parallax-background').length) {
+        $(".parallax-background").parallax();
+    }
   }
 }
 // Parallax
 $(document).ready(function(){
-    if ($('.parallax-background').length) {
-        
-        $(".parallax-background").parallax();
-    }
+    
 });
