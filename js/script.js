70dfcb2d1eea1c92cbc0188507762817bfe448b9
Vincent Tang <vincet3@uci.edu>
2017-07-20 13:46:27 -0700

    updated contact info

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+function email(){
+    var e = 'vincent';
+    e += 'poke';
+    e += 'tang';
+    e += '@';
+    e += 'gmail.com';
+    $('.email').text(e);
+    e = 'mailto:' + e;
+    $('.email ').attr('href', e);
+}
+function phone(){
+    var p = '626';
+    p += '-233';
+    p += '-2875';
+    $('.phone').text(p);
+}
+
 document.onreadystatechange = function () {
   var state = document.readyState;
   if (state == 'interactive') {
@@ -7,6 +24,8 @@ document.onreadystatechange = function () {
          document.getElementById('interactive');
          document.getElementById('preloader').style.visibility="hidden";
          document.getElementById('contents').style.visibility="visible";
+         email();
+         phone();
       },1000);
       if ($('.parallax-background').length) {
         $(".parallax-background").parallax();
